39ae4842b56d8397c5490cc23f6a1eaef640ce37
Zámbó Levente <zambo.levente@bardiauto.hu>
2026-08-05 17:02:09 +0200

    minor fixes and pull requests from upstream

--- xsd/onvif/onvif.go
+++ xsd/onvif/onvif.go
@@ -555,12 +555,12 @@ type PTZSpeed struct {
 type Vector2D struct {
 	X     float64    `xml:"x,attr"`
 	Y     float64    `xml:"y,attr"`
-	Space xsd.AnyURI `xml:"space,attr"`
+	Space xsd.AnyURI `xml:"space,attr,omitempty"`
 }
 
 type Vector1D struct {
 	X     float64    `xml:"x,attr"`
-	Space xsd.AnyURI `xml:"space,attr"`
+	Space xsd.AnyURI `xml:"space,attr,omitempty"`
 }
 
 type PanTiltLimits struct {
@@ -1013,8 +1013,8 @@ type PTZPreset struct {
 }
 
 type PTZVector struct {
-	PanTilt Vector2D `xml:"onvif:PanTilt"`
-	Zoom    Vector1D `xml:"onvif:Zoom"`
+	PanTilt Vector2D `xml:"PanTilt"`
+	Zoom    Vector1D `xml:"Zoom"`
 }
 
 type PTZStatus struct {
@@ -1835,9 +1835,9 @@ type GeoOrientation struct {
 }
 
 type FocusMove struct {
-	Absolute   AbsoluteFocus   `xml:"onvif:Absolute"`
-	Relative   RelativeFocus   `xml:"onvif:Relative"`
-	Continuous ContinuousFocus `xml:"onvif:Continuous"`
+	Absolute   *AbsoluteFocus   `xml:"onvif:Absolute,omitempty"`
+	Relative   *RelativeFocus   `xml:"onvif:Relative,omitempty"`
+	Continuous *ContinuousFocus `xml:"onvif:Continuous,omitempty"`
 }
 
 type ContinuousFocus struct {
